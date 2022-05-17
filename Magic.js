1283657e2ce5f1e27db96f28bcc85132063bd060
mittwoch01 <wenigwaves@gmail.com>
2022-05-17 15:06:55 +0900

    [feat]Magic.js : 배열을 이용해서 light event 만들기

--- Magic.js
+++ Magic.js
@@ -3,6 +3,10 @@ $(function(){ // 링크를 통해 jquery에 접근하는 방식.
     // variables
     $surface = $('.surface');
     $car = $('.car');
+    $img = $('.car img');
+    let flag = true;
+
+    const cars = ['./assets/Img_05.png','./assets/Img_06.png'];
 
     //keypress event
     $(document).on('keypress',function(e) {
@@ -14,7 +18,15 @@ $(function(){ // 링크를 통해 jquery에 접근하는 방식.
 
     // light
     $(document).on('keypress',function(e) {
-
+        if(e.which == 119){
+            if(flag){
+                flag = false;
+                $img.attr('src',cars[0]);
+            }else{
+                flag = true;
+                $img.attr('src',cars[1]);
+            }
+        }
     })
 
 });
